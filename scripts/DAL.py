af1df876ddfeb2d41b42a0d5860937e16cd3d029
jvcmtr <jvictorcmtr@gmail.com>
2024-02-18 14:53:24 -0300

    implemented Data Access script to read the raw data

---
+++ scripts/DAL.py
@@ -0,0 +1,24 @@
+import os
+
+__path = os.path.join(os.path.dirname(__file__), "../raw/")
+
+def read(file_name):
+    f = open( f"{__path}{file_name}" , "r", encoding="utf-8")
+    s = f.read()
+    f.close()
+    return s
+
+def name_key(name):
+    "defines the name of the property based on the name of the file"
+    name = name[:name.index(".")]    # Removes ".txt" , ".csv", ect... 
+    return name
+
+def get_module(module_name):
+    files = os.listdir(__path + module_name)
+    data = { "name" : module_name}
+    for file in files:
+        data[ name_key(file) ] = read( f"{module_name}/{file}" )
+    return data
+        
+base_module = get_module("base")
+print(base_module.keys())
